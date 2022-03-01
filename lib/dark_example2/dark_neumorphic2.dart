9fcd361def962f7cdfd2f26c9f9199d43b55334e
UTKARSH SHARMA <59551875+f1forhelp@users.noreply.github.com>
2022-03-01 13:12:43 +0530

    Some minor changes

---
+++ lib/dark_example2/dark_neumorphic2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DarkNeuMorphicExample2 extends StatefulWidget {
+  const DarkNeuMorphicExample2({Key? key}) : super(key: key);
+
+  @override
+  _DarkNeuMorphicExample2State createState() => _DarkNeuMorphicExample2State();
+}
+
+class _DarkNeuMorphicExample2State extends State<DarkNeuMorphicExample2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
